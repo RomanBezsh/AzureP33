d924f503e6ca4176a77556fd92803b4f9715fb16
RomanBezsh <antikys189@gmail.com>
2025-12-19 17:34:42 +0200

    innit commit

--- AzureP33/wwwroot/js/site.js
+++ AzureP33/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const btnSwapLanguages = document.getElementById("btn-swap-languages");
+
+btnSwapLanguages.addEventListener("click", () => swapLanguages());
+
+const swapLanguages = () => {
+    const langFrom = document.getElementsByName("lang-from")[0];
+    const langTo = document.getElementsByName("lang-to")[0];
+
+    const temp = langFrom.value;
+    langFrom.value = langTo.value;
+    langTo.value = temp;
+};
